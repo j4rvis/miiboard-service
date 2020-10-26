c6b0bf71cea74e20fc39fba8a7833d63fbf7cf72
Micha <schwarzm90@gmail.com>
2020-10-26 09:27:31 +0100

    prepare repo

--- usecase/dashboard.go
+++ usecase/dashboard.go
@@ -1,5 +1,22 @@
 package usecase
 
+import (
+	"miiboard-service/model"
+	"miiboard-service/repository"
+)
+
 type DashboardUsecase interface {
-	GetByID(id int64)
+	GetByID(id int64) (model.Dashboard, error)
+}
+
+type dashboardUseCases struct {
+	dashboards repository.DashboardRepository
+}
+
+func NewDashboardUseCase(repository repository.DashboardRepository) DashboardUsecase {
+	return &dashboardUseCases{repository}
+}
+
+func (d DashboardUsecase) GetByID(id int64) (model.Dashboard, error) {
+
 }
